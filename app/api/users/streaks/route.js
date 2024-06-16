f201029152e5b8679b7f1c9b19c518715cd092ed
MartinFid <martin.fidon@gmail.com>
2024-06-16 13:26:51 +0200

    chore: Add route for getting user streak

---
+++ app/api/users/streaks/route.js
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server';
+import prisma from '@/lib/supabase';
+import { getToken } from "next-auth/jwt"
+
+export async function GET(req) {
+    
+    // Verify if the request comes from an authenticated user
+    const token = await getToken({ req })
+    
+    if (!token) {
+        return NextResponse.json({ message: 'User not connected' }, { status: 401 }); // Return error 401 if user unauthenticated
+    }
+
+    // Get the authenticated user
+    try {
+        const user = await prisma.users.findUnique({
+            where: {
+                username: token.user.username,
+            },
+        }).catch((error) => {
+            console.log(error);
+            throw error;
+        });
+        
+        if(!user) return NextResponse.json({ message: 'User not found' }, { status: 404 });
+
+        // Return user.streak
+        return NextResponse.json(user.streak);
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message : 'Error looking for user' }, { status: 500 });
+    }
+
+
+}
