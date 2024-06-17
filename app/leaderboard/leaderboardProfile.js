9d76f58876ff08c374b84581433c844227a8000c
Ludovick <ludovick.laine@gmail.com>
2024-06-17 14:29:02 +0200

    200 au lieu de 300 enfaite

--- app/leaderboard/leaderboardProfile.js
+++ app/leaderboard/leaderboardProfile.js
@@ -14,27 +14,27 @@ import {
 
 
     return (
-        <div className={`flex w-full items-center gap-5 rounded-2xl px-5 py-2 md:mx-0 ${condition ? 'bg-purple-300' : ' hover:bg-gray-100'}`}>
-        <div className="flex items-center gap-4">
-          {place === 1 ? (
-            <FirstPlaceSvg />
-          ) : place === 2 ? (
-            <SecondPlaceSvg />
-          ) : place === 3 ? (
-            <ThirdPlaceSvg />
-          ) : (
-            <div className="flex h-10 w-10 items-center justify-center font-bold text-purple-600">
-              {place}
+        <div className={`flex w-full items-center gap-5 rounded-2xl px-5 py-2 md:mx-0 ${condition ? 'bg-purple-200' : ' hover:bg-gray-100'}`}>
+            <div className="flex items-center gap-4">
+            {place === 1 ? (
+                <FirstPlaceSvg />
+            ) : place === 2 ? (
+                <SecondPlaceSvg />
+            ) : place === 3 ? (
+                <ThirdPlaceSvg />
+            ) : (
+                <div className="flex h-10 w-10 items-center justify-center font-bold text-purple-600">
+                {place}
+                </div>
+            )}
             </div>
-          )}
-        </div>
 
-        <div className="grow overflow-hidden overflow-ellipsis font-bold text-center">
-            {username}
-        </div>
-        <div className="shrink-0 text-right text-indigo-500">
-            {`${xp} XP`}
-        </div>
+            <div className="grow overflow-hidden overflow-ellipsis font-bold text-center">
+                {username}
+            </div>
+            <div className="shrink-0 text-right text-indigo-500">
+                {`${xp} XP`}
+            </div>
       </div>
     );
   };
