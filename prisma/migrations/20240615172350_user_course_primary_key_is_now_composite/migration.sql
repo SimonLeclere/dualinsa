4f7fe63f0683f68a41667a58be086e4a5cc2c56a
Simon <simon-leclere@orange.fr>
2024-06-17 08:25:53 +0200

    db modifications

---
+++ prisma/migrations/20240615172350_user_course_primary_key_is_now_composite/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The primary key for the `UserCourse` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `id` on the `UserCourse` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Courses" ALTER COLUMN "celeneLink" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "UserCourse" DROP CONSTRAINT "UserCourse_pkey",
+DROP COLUMN "id",
+ADD CONSTRAINT "UserCourse_pkey" PRIMARY KEY ("userId", "courseId");
