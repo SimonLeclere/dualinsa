226dbd472b10ba48d3ccf8184dea78374d2859e7
MartinFid <martin.fidon@gmail.com>
2024-06-19 23:18:24 +0200

    chore: Update CoursePage to fetch course data and user advancement

--- app/courses/[id]/page.js
+++ app/courses/[id]/page.js
@@ -1,7 +1,19 @@
+"use client";
+
+import useSwr from "swr";
+
 import NavBar from "/app/components/NavBar";
 import BottomBar from "/app/components/BottomBar";
 
 export default function CoursePage() {
+
+    const {data, error, isLoading} = useSwr('/api/courses', (url) => fetch(url).then(res => res.json()));
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
+    const units = data?.units; // All units from the course with all their checkpoints
+    const advancement = data?.advancement; // User advancement in the course
+    // advancement.currentUnitIndex & advancement.currentCheckpointIndex
+
     return (
         <div className="flex flex-col h-screen">
             <NavBar />
