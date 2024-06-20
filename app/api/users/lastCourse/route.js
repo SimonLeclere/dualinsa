5bd90f7434f823091da3ccc7d83af09d41e4c803
MartinFid <martin.fidon@gmail.com>
2024-06-20 13:18:03 +0200

    chore: Implement API routes for retrieving and updating lastCourse

---
+++ app/api/users/lastCourse/route.js
@@ -0,0 +1,76 @@
+import { NextResponse } from 'next/server';
+import prisma from '/lib/supabase';
+import { getToken } from "next-auth/jwt"
+
+export async function GET(req) {
+    
+    // Verify if the request comes from an authenticated user
+    const token = await getToken({ req })
+    
+    if (!token) {
+        return NextResponse.json({ message: 'User not connected' }, { status: 401 }); // Return error 401 if user unauthenticated
+    }
+
+    // Get the authenticated user
+    try {
+        const lastCourse = await prisma.users.findUnique({
+            select : {
+                lastCourse: true,
+            },
+            where: {
+                id: token.user.id,
+            },
+        }).catch((error) => {
+            console.log(error);
+            throw error;
+        });
+        
+        if(!lastCourse) return NextResponse.json({ message: 'User not found' }, { status: 404 });
+
+        // Return user.lastCourse
+        return NextResponse.json(lastCourse, { status: 200 });
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message : 'Error looking for user' }, { status: 500 });
+    }
+}
+
+export async function POST(req) {
+    const token = await getToken({ req })
+    
+    if (!token) {
+        return NextResponse.json({ message: 'User not connected' }, { status: 401 }); // Return error 401 if user unauthenticated
+    }
+
+    // Get the authenticated user
+    try {
+        const user = await prisma.users.findUnique({
+            where: {
+                id: token.user.id,
+            },
+        }).catch((error) => {
+            console.log(error);
+            throw error;
+        });
+        
+        if(!user) return NextResponse.json({ message: 'User not found' }, { status: 404 });
+
+        // Update user.lastCourse
+        const { lastCourse } = JSON.parse(req.body);
+        const updatedUser = await prisma.users.update({
+            where: { id: token.user.id },
+            data: { lastCourse },
+        }).catch((error) => {
+            console.log(error);
+            throw error;
+        });
+
+        // Return updated user
+        return NextResponse.json(updatedUser, { status: 200 });
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message : 'Error setting user.lastCourse' }, { status: 500 });
+    }
+}
