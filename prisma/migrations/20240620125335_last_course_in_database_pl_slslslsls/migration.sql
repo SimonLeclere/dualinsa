b216fd4acd8ffd0482455c0f85db8cd313413ecd
MartinFid <martin.fidon@gmail.com>
2024-06-20 14:53:43 +0200

    chore: Update UserCourse model to replace currentUnitCheckpointProgress with currentCheckpointProgress and add lastCourse field to Users model

---
+++ prisma/migrations/20240620125335_last_course_in_database_pl_slslslsls/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `currentUnitCheckpointProgress` on the `UserCourse` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "UserCourse" DROP COLUMN "currentUnitCheckpointProgress",
+ADD COLUMN     "currentCheckpointProgress" INTEGER NOT NULL DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "Users" ADD COLUMN     "lastCourse" INTEGER NOT NULL DEFAULT 0;
