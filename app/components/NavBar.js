0312fbc987064da6be8c1a3efee9a7ec6136f88a
maximebgd <max.begoud@gmail.com>
2024-06-20 12:24:02 +0200

    upgrade top navBar

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,94 +1,116 @@
-'use client';
+"use client";
 
 import { useState } from "react";
 import Link from "next/link";
 
-import Calendar from "@/components/Calendar"
+import Calendar from "@/components/Calendar";
 
 import FireSvg from "@/components/icons/FireSvg";
 import GemSvg from "@/components/icons/GemSvg";
 import LingotsTreasureChestSvg from "@/components/icons/LingotsTreasureChestSvg";
+import { ProgressSection } from "./ProgessSection";
 
 // type MenuState = "HIDDEN" | "STREAK" | "GEMS";
 
-export default function NavBar({ backgroundColor = "bg-purple-400", borderColor = "border-purple-500" }) {
+export default function NavBar({
+  backgroundColor = "bg-purple-400",
+  borderColor = "border-purple-500",
+}) {
+  const [menu, setMenu] = useState("HIDDEN");
+  const [streak, setStreak] = useState(0);
+  const [lingots, setLingots] = useState(0);
 
-    const [menu, setMenu] = useState("HIDDEN");
-    const [streak, setStreak] = useState(0);
-    const [lingots, setLingots] = useState(0);
+  return (
+    <header className="fixed z-20 h-[58px] w-full">
+      <div
+        className={`relative flex h-full w-full items-center justify-around border-b-2 px-[10px] transition duration-500 md:hidden ${borderColor} ${backgroundColor}`}
+      >
+        <button
+          className="flex items-center gap-2 font-bold text-white"
+          onClick={() => setMenu((x) => (x === "STREAK" ? "HIDDEN" : "STREAK"))}
+        >
+          <FireSvg empty={streak === 0} />
+          <span className={streak > 0 ? "text-white" : "text-black opacity-20"}>
+            {streak}
+          </span>
+        </button>
 
-    return (
-        <header className="fixed z-20 h-[58px] w-full">
-            <div className={`relative flex h-full w-full items-center justify-around border-b-2 px-[10px] transition duration-500 md:hidden ${borderColor} ${backgroundColor}`}>
+        <button
+          className="flex items-center gap-2 font-bold"
+          onClick={() => setMenu((x) => (x === "GEMS" ? "HIDDEN" : "GEMS"))}
+        >
+          <GemSvg empty={lingots === 0} />
+          <span
+            className={lingots > 0 ? "text-white" : "text-black opacity-20"}
+          >
+            {lingots}
+          </span>
+        </button>
 
-                <button
-                    className="flex items-center gap-2 font-bold text-white"
-                    onClick={() => setMenu((x) => (x === "STREAK" ? "HIDDEN" : "STREAK"))}
-                >
-                    <FireSvg empty={streak === 0} />
-                    <span className={streak > 0 ? "text-white" : "text-black opacity-20"}>{streak}</span>
-                </button>
+        {/* DROPDOWN MENU */}
+        <div
+          className={`absolute left-0 right-0 top-full bg-white transition duration-300 ${
+            menu === "HIDDEN" ? "opacity-0" : "opacity-100"
+          }`}
+        >
+          {(() => {
+            switch (menu) {
+              case "STREAK":
+                return (
+                  <div className="flex grow flex-col items-center gap-3 p-5 border-b-2 border-gray-300">
+                    <h2 className="text-xl font-bold">Streak</h2>
+                    <p className="text-sm text-gray-400">
+                      {`Practice each day so your streak won't reset!`}
+                    </p>
+                    <div className="self-stretch">
+                      <Calendar />
+                    </div>
+                  </div>
+                );
 
-                <button
-                    className="flex items-center gap-2 font-bold"
-                    onClick={() => setMenu((x) => (x === "GEMS" ? "HIDDEN" : "GEMS"))}
-                >
-                    <GemSvg empty={lingots === 0}/>
-                    <span className={lingots > 0 ? "text-white" : "text-black opacity-20"}>{lingots}</span>
-                </button>
+              case "GEMS":
+                return (
+                  <>
+                    <div className="flex grow items-center gap-3 p-5 border-b-2 border-gray-300">
+                      <LingotsTreasureChestSvg className="h-20 w-20" />
+                      <div className="flex flex-col gap-3">
+                        <h2 className="text-xl font-bold text-black">
+                          Lingots
+                        </h2>
+                        <p className="text-sm font-normal text-gray-400">
+                          You have {lingots}{" "}
+                          {lingots === 1 ? "lingot" : "lingots"}.
+                        </p>
+                        <Link
+                          className="font-bold uppercase text-blue-400 transition hover:brightness-110"
+                          href="/shop"
+                        >
+                          Go to shop
+                        </Link>
+                      </div>
+                    </div>
 
-                {/* DROPDOWN MENU */}
-                <div className={`absolute left-0 right-0 top-full bg-white transition duration-300 ${menu === "HIDDEN" ? "opacity-0" : "opacity-100"}`}>
-                    {(() => {
-                        switch (menu) {
-                            case "STREAK":
-                                return (
-                                    <div className="flex grow flex-col items-center gap-3 p-5">
-                                        <h2 className="text-xl font-bold">Streak</h2>
-                                        <p className="text-sm text-gray-400">
-                                            {`Practice each day so your streak won't reset!`}
-                                        </p>
-                                        <div className="self-stretch">
-                                            <Calendar />
-                                        </div>
-                                    </div>
-                                );
+                    <div className="flex grow items-center gap-3 p-5 border-b-2 border-gray-300">
+                      <ProgressSection forNavBar={true} />
+                    </div>
+                  </>
+                );
 
-                            case "GEMS":
-                                return (
-                                    <div className="flex grow items-center gap-3 p-5">
-                                        <LingotsTreasureChestSvg className="h-24 w-24" />
-                                        <div className="flex flex-col gap-3">
-                                            <h2 className="text-xl font-bold text-black">Lingots</h2>
-                                            <p className="text-sm font-normal text-gray-400">
-                                                You have {lingots}{" "}
-                                                {lingots === 1 ? "lingot" : "lingots"}.
-                                            </p>
-                                            <Link
-                                                className="font-bold uppercase text-blue-400 transition hover:brightness-110"
-                                                href="/shop"
-                                            >
-                                                Go to shop
-                                            </Link>
-                                        </div>
-                                    </div>
-                                );
-
-                            case "HIDDEN":
-                                return null;
-                        }
-                    })()}
-                    <div
-                        className={[
-                            "absolute left-0 top-full h-screen w-screen bg-black opacity-30",
-                            menu === "HIDDEN" ? "pointer-events-none" : "",
-                        ].join(" ")}
-                        onClick={() => setMenu("HIDDEN")}
-                        aria-label="Hide menu"
-                        role="button"
-                    ></div>
-                </div>
-            </div>
-        </header>
-    );
-};
+              case "HIDDEN":
+                return null;
+            }
+          })()}
+          <div
+            className={[
+              "absolute left-0 top-full h-screen w-screen bg-black opacity-30 z-10",
+              menu === "HIDDEN" ? "pointer-events-none" : "",
+            ].join(" ")}
+            onClick={() => setMenu("HIDDEN")}
+            aria-label="Hide menu"
+            role="button"
+          ></div>
+        </div>
+      </div>
+    </header>
+  );
+}
--- app/components/ProgessSection.js
+++ app/components/ProgessSection.js
@@ -2,39 +2,80 @@ import Link from "next/link";
 
 import { LightningProgressSvg } from "../components/icons/LightningProgressSvg";
 
-export const ProgressSection = () => {
+export const ProgressSection = ({ forNavBar = false }) => {
   const xpToday = 14; // TODO: get from API
   const goalXp = 100; // TODO: get from API
-  return (
-    <article className="flex flex-col gap-5 rounded-2xl border-2 border-gray-200 p-6 font-bold text-gray-700">
-      <div className="flex items-center justify-between">
-        <h2 className="text-xl">Daily Progression</h2>
-        <Link href="/settings/coach" className="uppercase text-blue-400">
-          Modifier
-        </Link>
-      </div>
-      
-      <div className="flex gap-5">
-        <LightningProgressSvg />
-        <div className="flex grow flex-col justify-around">
-          <h3 className="font-normal text-gray-500">Objectif {goalXp} XP</h3>
-          <div className="flex items-center gap-5">
-            <div className="relative h-4 w-full grow rounded-full bg-gray-200">
-              {xpToday > 0 && (
-                <div
-                  className="absolute left-0 top-0 h-4 rounded-full bg-yellow-400"
-                  style={{ width: `${Math.min(1, xpToday / goalXp) * 100}%` }}
-                >
-                  <div className="absolute left-2 right-2 top-1 h-[6px] rounded-full bg-yellow-300"></div>
-                </div>
-              )}
-            </div>
-            <div className="text-md shrink-0 font-normal text-gray-400">
-              {xpToday}/{goalXp} XP
+
+  if (forNavBar === false) {
+    // Affichage dans RightBar
+    return (
+      <article className="flex flex-col gap-5 rounded-2xl border-2 border-gray-200 p-6 font-bold text-gray-700">
+        <div className="flex items-center justify-between">
+          <h2 className="text-xl">Daily Progression</h2>
+          <Link href="/settings/coach" className="uppercase text-blue-400">
+            Modifier
+          </Link>
+        </div>
+
+        <div className="flex gap-5">
+          <LightningProgressSvg />
+          <div className="flex grow flex-col justify-around">
+            <h3 className="font-normal text-gray-500">Objectif {goalXp} XP</h3>
+            <div className="flex items-center gap-5">
+              <div className="relative h-4 w-full grow rounded-full bg-gray-200">
+                {xpToday > 0 && (
+                  <div
+                    className="absolute left-0 top-0 h-4 rounded-full bg-yellow-400"
+                    style={{ width: `${Math.min(1, xpToday / goalXp) * 100}%` }}
+                  >
+                    <div className="absolute left-2 right-2 top-1 h-[6px] rounded-full bg-yellow-300"></div>
+                  </div>
+                )}
+              </div>
+              <div className="text-md shrink-0 font-normal text-gray-400">
+                {xpToday}/{goalXp} XP
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </article>
-  );
+      </article>
+    );
+  } else {
+    // Affichage dans NavBar
+    return (
+      <>
+        <LightningProgressSvg size={80} />
+        <div className="flex flex-col gap-3">
+          <h2 className="text-xl font-bold text-black">Daily Progression</h2>
+          <p className="text-sm font-normal text-gray-400">
+            <div className="flex grow flex-col justify-around">
+              <div className="flex items-center gap-5 h-5 w-64">
+                <div className="relative h-4 w-full grow rounded-full bg-gray-200">
+                  {xpToday > 0 && (
+                    <div
+                      className="absolute left-0 top-0 h-4 rounded-full bg-yellow-400"
+                      style={{
+                        width: `${Math.min(1, xpToday / goalXp) * 100}%`,
+                      }}
+                    >
+                      <div className="absolute left-2 right-2 top-1 h-[6px] rounded-full bg-yellow-300"></div>
+                    </div>
+                  )}
+                </div>
+                <div className="text-md shrink-0 font-normal text-gray-400">
+                  {xpToday}/{goalXp} XP
+                </div>
+              </div>
+            </div>
+          </p>
+          <Link
+            className="font-bold uppercase text-blue-400 w-auto transition hover:brightness-110"
+            href="/settings/coach"
+          >
+            Modifier
+          </Link>
+        </div>
+      </>
+    );
+  }
 };
